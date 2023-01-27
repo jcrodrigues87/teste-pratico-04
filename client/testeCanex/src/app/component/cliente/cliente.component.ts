417b8ebc8b90bae9843ec84feafdbf1f5bed6d92
Matheus Miranda <matheusfelipemiranda1@gmail.com>
2023-01-27 10:56:24 -0300

    Adição da busca de CEP

--- client/testeCanex/src/app/component/cliente/cliente.component.ts
+++ client/testeCanex/src/app/component/cliente/cliente.component.ts
@@ -1,3 +1,4 @@
+import { BuscaCepService } from './../../service/busca-cep.service';
 import { ClienteService } from 'src/app/service/cliente.service';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Cliente } from 'src/app/models/cliente.model';
@@ -17,8 +18,20 @@ export class ClienteComponent implements OnInit {
   email: string = '';
   cep: string = '';
   endereco: string = '';
+  resultado: any = {
+    cep: "",
+    logradouro: "",
+    complemento: "",
+    bairro: "",
+    localidade: "",
+    uf: "",
+    ibge: "",
+    gia: "",
+    ddd: "",
+    siafi: ""
+  };
 
-  constructor(private service: ClienteService) {}
+  constructor(private service: ClienteService, private busca: BuscaCepService) {}
 
   ngOnInit() {
     // this.testar();
@@ -40,9 +53,10 @@ export class ClienteComponent implements OnInit {
 
     this.service.salvar(cliente).subscribe((resultado) => {
 
+      this.mostraMensagem("Cliente salvo com sucesso!", "success")
       this.limpar();
     },
-    error => console.log(error)
+    error => {console.log(error); this.mostraMensagem("Não foi possível salvar o cliente", "danger");}
     );
   }
 
@@ -81,9 +95,10 @@ export class ClienteComponent implements OnInit {
 
     this.service.alterar(cliente).subscribe((resultado) => {
 
+      this.mostraMensagem("Cliente alterado com sucesso!", "success")
       this.limpar();
     },
-    error => console.log(error)
+    error => {console.log(error); this.mostraMensagem("Não foi possível alterar o cliente", "danger")}
     );
   }
 
@@ -91,9 +106,49 @@ export class ClienteComponent implements OnInit {
     let resultado = this.service.pesquisaPorEmail(this.email)
     .subscribe(
       resultado => resultado,
-      error => console.log(error)
+      error => {console.log(error); this.mostraMensagem("Não foi possível pesquisar o e-mail " + this.email, "danger")}
     );
     console.log(resultado);
 
   }
+
+  buscar(){
+
+    let subscriber = this.busca.buscar(this.cep);
+
+    this.endereco = "";
+
+    subscriber.subscribe(resultado => {
+      this.resultado = resultado;
+      if (this.resultado.logradouro !== "") this.endereco += this.resultado.logradouro + ", ";
+      if (this.resultado.complemento !== "") this.endereco += this.resultado.complemento + ", ";
+      if (this.resultado.bairro !== "") this.endereco += this.resultado.bairro + ", ";
+      if (this.resultado.localidade !== "") this.endereco += this.resultado.localidade + ", ";
+      if (this.resultado.uf !== "") this.endereco += this.resultado.uf;
+    }, erro => {console.log(erro); this.mostraMensagem("Não foi possível buscar o cep " + this.cep, "danger")});
+
+  }
+
+  mostraMensagem(mensagem: string, nivel: string){
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+    const alert = (message: string, level: string) => {
+      const wrapper = document.createElement('div')
+      wrapper.innerHTML = [
+        `<div class="alert alert-${level} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '</div>'
+      ].join('')
+
+      alertPlaceholder.append(wrapper)
+
+      setTimeout(() => {
+        wrapper.remove();
+      }, 3000);
+    }
+
+    alert(mensagem, nivel)
+
+  }
+
 }

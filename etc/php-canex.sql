43b01524d201c3fd5aa3fa25754cf23a152e7242
Luciano <luciano.vittoretti@gmail.com>
2023-03-17 13:59:12 -0300

    sql para criação do banco de dados

---
+++ etc/php-canex.sql
@@ -0,0 +1,136 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 17, 2023 at 05:52 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php-canex`
+--
+CREATE DATABASE IF NOT EXISTS `php-canex` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `php-canex`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contatos`
+--
+
+CREATE TABLE `contatos` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `fornecedores_id` int(11) UNSIGNED NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `departamento` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documentos`
+--
+
+CREATE TABLE `documentos` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `fornecedores_id` int(11) UNSIGNED NOT NULL,
+  `path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fornecedores`
+--
+
+CREATE TABLE `fornecedores` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `cnpj` varchar(18) NOT NULL,
+  `razao_social` varchar(255) NOT NULL,
+  `data_abertura` date NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `cep` varchar(10) NOT NULL,
+  `endereco` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contatos`
+--
+ALTER TABLE `contatos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fornecedores_id` (`fornecedores_id`);
+
+--
+-- Indexes for table `documentos`
+--
+ALTER TABLE `documentos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fornecedores_id` (`fornecedores_id`);
+
+--
+-- Indexes for table `fornecedores`
+--
+ALTER TABLE `fornecedores`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `cnpj` (`cnpj`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contatos`
+--
+ALTER TABLE `contatos`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `documentos`
+--
+ALTER TABLE `documentos`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `fornecedores`
+--
+ALTER TABLE `fornecedores`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `contatos`
+--
+ALTER TABLE `contatos`
+  ADD CONSTRAINT `contatos_ibfk_1` FOREIGN KEY (`fornecedores_id`) REFERENCES `fornecedores` (`id`);
+
+--
+-- Constraints for table `documentos`
+--
+ALTER TABLE `documentos`
+  ADD CONSTRAINT `documentos_ibfk_1` FOREIGN KEY (`fornecedores_id`) REFERENCES `fornecedores` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

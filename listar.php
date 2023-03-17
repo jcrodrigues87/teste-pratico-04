4fc2c12b81d3f5daab9bd0f5369799b63d7e7785
Luciano <luciano.vittoretti@gmail.com>
2023-03-17 13:58:36 -0300

    corrigindo forma de carregar os documentos e contatos na lista principal

--- listar.php
+++ listar.php
@@ -5,13 +5,9 @@ include("inc/header.html");
 
 // query sql listando todos os dados de fornecedores e quantidade de docs e contatos
 $query = "
-    SELECT DISTINCT
-        fornecedores.*,
-        count(contatos.id) AS quantidade_contatos,
-        count(documentos.id) AS quantidade_documentos
+    SELECT
+        *
     FROM fornecedores
-        LEFT JOIN documentos ON documentos.fornecedores_id = fornecedores.id
-        LEFT JOIN contatos ON contatos.fornecedores_id = fornecedores.id
         ";
 
 // buscando fornecedores para listar
@@ -20,8 +16,35 @@ $fornecedores_result = mysqli_query($mysqli, $query);
 // convertendo resultado em um array
 $fornecedores = [];
 while ($fornecedor = mysqli_fetch_assoc($fornecedores_result)) {
+    // recuperando id do fornecedor
+    $fornecedor_id = $fornecedor["id"];
+
     // aplicando htmlentities para exibição apropriada no HTML
-    $fornecedores[] = array_map("htmlentities", $fornecedor);
+    $fornecedores[$fornecedor_id] = array_map("htmlentities", $fornecedor);
+
+    // recuperando quantidade de contatos que esse fornecedor tem
+    $query = "
+        SELECT
+            count(*)
+        FROM contatos
+        WHERE fornecedores_id = $fornecedor_id
+        ";
+
+    $contatos_result = mysqli_query($mysqli, $query);
+    $contatos = mysqli_fetch_row($contatos_result);
+    $fornecedores[$fornecedor_id]["quantidade_contatos"] = $contatos[0];
+
+    // recuperando quantidade de documentos que esse fornecedor tem
+    $query = "
+        SELECT
+            count(*)
+        FROM documentos
+        WHERE fornecedores_id = $fornecedor_id
+        ";
+
+    $documentos_result = mysqli_query($mysqli, $query);
+    $documentos = mysqli_fetch_row($documentos_result);
+    $fornecedores[$fornecedor_id]["quantidade_documentos"] = $documentos[0];
 }
 
 ?>
@@ -30,18 +53,18 @@ while ($fornecedor = mysqli_fetch_assoc($fornecedores_result)) {
 
 <table class="lista">
     <tr>
-        <td>CNPJ</td>
-        <td>Razão Social</td>
-        <td>Data de Abertura</td>
-        <td>Telefone</td>
-        <td>E-mail</td>
-        <td>CEP</td>
-        <td>Endereço</td>
-        <td>Contatos</td>
-        <td>Documentos</td>
+        <th>CNPJ</th>
+        <th>Razão Social</th>
+        <th>Data de Abertura</th>
+        <th>Telefone</th>
+        <th>E-mail</th>
+        <th>CEP</th>
+        <th>Endereço</th>
+        <th>Contatos</th>
+        <th>Documentos</th>
     </tr>
     <?php
-    foreach ($fornecedores as $fornecedor) {
+    foreach ($fornecedores as $fornecedor_id => $fornecedor) {
     ?>
         <tr>
             <td><?= $fornecedor["cnpj"] ?></td>
@@ -51,8 +74,20 @@ while ($fornecedor = mysqli_fetch_assoc($fornecedores_result)) {
             <td><?= $fornecedor["email"] ?></td>
             <td><?= $fornecedor["cep"] ?></td>
             <td><?= $fornecedor["endereco"] ?></td>
-            <td><?= $fornecedor["quantidade_contatos"] ?></td>
-            <td><?= $fornecedor["quantidade_documentos"] ?></td>
+            <td>
+                <?php
+                echo "<a href='contatos.php?fornecedores_id=$fornecedor_id'>";
+                echo $fornecedor["quantidade_contatos"];
+                echo "</a>";
+                ?>
+            </td>
+            <td>
+                <?php
+                echo "<a href='documentos.php?fornecedores_id=$fornecedor_id'>";
+                echo $fornecedor["quantidade_documentos"];
+                echo "</a>";
+                ?>
+            </td>
         </tr>
     <?php
     }

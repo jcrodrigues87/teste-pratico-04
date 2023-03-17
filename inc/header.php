ad44da800e7f2cfbd3ac5f5a57928e6df637c7ca
Luciano <luciano.vittoretti@gmail.com>
2023-03-17 19:26:55 -0300

    prevenindo cache da tela de lista

--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,17 @@
 <?php
 
+header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Connection: close");
+
 // criando conexão com o servidor mysql
 $mysqli = mysqli_connect("localhost", "root", "");
 
 // selecionando o bd
 mysqli_select_db($mysqli, "php-canex");
+//mysqli_select_db($mysqli, "php_canex");
 
 ?>

c6b5a00c969bc8f8180ad4877350d319d65a497d
Luciano <luciano.vittoretti@gmail.com>
2023-03-18 11:17:20 -0300

    comentando problema de cache

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,7 @@
 <?php
 
+// pagina de listar não atualiza, forçando a apertar F5 para ver lista atualizada
+// esse codigo abaixo força o navegador a não fazer cache para evitar esse problema
 header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
@@ -12,6 +14,5 @@ $mysqli = mysqli_connect("localhost", "root", "");
 
 // selecionando o bd
 mysqli_select_db($mysqli, "php-canex");
-//mysqli_select_db($mysqli, "php_canex");
 
 ?>
